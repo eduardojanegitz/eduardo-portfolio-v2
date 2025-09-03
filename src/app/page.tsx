335fdf0c5704a46b0cb81430c6e36064da5ead7e
Eduardo Alves <edu0alves00@gmail.com>
2025-09-02 23:31:43 -0300

    feat: imported new sections from the page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
+import { AboutSection } from "@/sections/About";
+import { ContactSection } from "@/sections/Contact";
 import { Header } from "@/sections/Header";
 import { HeroSection } from "@/sections/Hero";
 import { ProjectsSection } from "@/sections/Projects";
 import { TapeSection } from "@/sections/Tape";
+import { TestimonialsSection } from "@/sections/Testimonials";
 
 export default function Home() {
   return (
@@ -11,6 +14,9 @@ export default function Home() {
         <HeroSection />
         <ProjectsSection />
         <TapeSection />
+        <TestimonialsSection />
+        <AboutSection />
+        <ContactSection />
       </h1>
     </div>
   );
