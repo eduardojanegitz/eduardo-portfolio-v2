0f90447731c4d2e6238828653761d2d270acfe32
Eduardo Alves <edu0alves00@gmail.com>
2025-09-20 18:36:11 -0300

    fix: Comentado seção de testimonials momentaneamente

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,7 +15,7 @@ export default function Home() {
         <HeroSection />
         <ProjectsSection />
         <TapeSection />
-        <TestimonialsSection />
+        {/* <TestimonialsSection /> */}
         <AboutSection />
         <ContactSection />
         <Footer />
