de93b270aba35ac1c4070a18a1d419b65d60ab9d
Eduardo Alves <edu0alves00@gmail.com>
2025-09-20 18:38:24 -0300

    fix: Ajustado footer

--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -1,45 +1,40 @@
 import ArrowUpRightIcon from "@/assets/icons/arrow-up-right.svg";
 
 const footerLinks = [
-  {
-    title: "LinkedIn",
-    href: "https://www.linkedin.com/in/eduardo-alves-091aa1219/",
-  },
-  {
-    title: "Github",
-    href: "https://github.com/eduardojanegitz",
-  },
-  {
-    title: "Instagram",
-    href: "https://www.instagram.com/eduualvs/",
-  },
+  { title: "LinkedIn", href: "https://www.linkedin.com/in/eduardo-alves-091aa1219/" },
+  { title: "Github", href: "https://github.com/eduardojanegitz" },
+  { title: "Instagram", href: "https://www.instagram.com/eduualvs/" },
 ];
 
 export const Footer = () => {
   return (
-    <footer className="relative -z-10 overflow-x-clip">
+    // REMOVER -z-10 do footer
+    <footer className="relative overflow-x-clip">
       <div
-        className="absolute h-[400px] w-[1600px] 
+        className="pointer-events-none absolute h-[400px] w-[1600px] 
         bottom-0 left-1/2 -translate-x-1/2 bg-emerald-300/30
         [mask-image:radial-gradient(50%_50%_at_bottom_center,black,transparent)] -z-10"
       />
       <div className="container">
         <div
           className="border-t border-white/15 py-6 
-        text-sm flex flex-col md:flex-row md:justify-between items-center gap-8"
+          text-sm flex flex-col md:flex-row md:justify-between items-center gap-8"
         >
           <div className="text-white/40">
             &copy; 2025. Todos os direitos reservados.
           </div>
+
           <nav className="flex flex-col items-center gap-8 md:flex-row">
             {footerLinks.map((link) => (
               <a
                 href={link.href}
                 key={link.title}
-                className="inline-flex items-center gap-1.5"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="inline-flex items-center gap-1.5 hover:underline"
               >
                 <span className="font-semibold">{link.title}</span>
-                <ArrowUpRightIcon size="4" />
+                <ArrowUpRightIcon className="size-4" />
               </a>
             ))}
           </nav>
